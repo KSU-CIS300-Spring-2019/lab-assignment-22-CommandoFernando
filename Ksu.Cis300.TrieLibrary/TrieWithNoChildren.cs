c69af9a2f9b9970d86fe4adf363dd5c476a0b496
fvelarde <f.velade@hotmail.com>
2019-03-25 12:14:11 -0500

    Done!

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// Indicates whether the trie rooted at this node contains the empty string.
+        /// </summary>
+        private bool _hasEmptyString = false;
+
+        /// <summary>
+        /// Adds the given string to the trie rooted at this node.
+        /// </summary>
+        /// <param name="s">The string to add.</param>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                ITrie _child = new TrieWithOneChild(s, _hasEmptyString);
+                return _child;
+                //_children[loc].Add(s.Substring(1));
+            }
+            return this;
+            //The Add method for TrieWithNoChildren will need to handle the empty string 
+            //in the same way as the above Add method. However, this implementation cannot 
+            //store a nonempty string. In this case, it will need to construct and return a 
+            //new TrieWithOneChild containing the string to be added and the bool stored in this node.
+        }
+
+        /// <summary>
+        /// Gets whether the trie rooted at this node contains the given string.
+        /// </summary>
+        /// <param name="s">The string to look up.</param>
+        /// <returns>Whether the trie rooted at this node contains s.</returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            return false;
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// Adds the given string to the trie rooted at this node.
+        /// </summary>
+        /// <param name="s">The string to add.</param>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _hasEmptyString = true;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                if (s[0] != _childLabel)
+                {
+                    return new TrieWithManyChildren(s, _hasEmptyString, _childLabel, _child);
+                }
+                _child = _child.Add(s.Substring(1));
+                //_children[loc].Add(s.Substring(1));
+            }
+            return this;
+        }
+        /// <summary>
+        /// Gets whether the trie rooted at this node contains the given string.
+        /// </summary>
+        /// <param name="s">The string to look up.</param>
+        /// <returns>Whether the trie rooted at this node contains s.</returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _hasEmptyString;
+            }
+            else
+            {
+                //int loc = s[0] - 'a';
+                if (s[0] != _childLabel)
+                {
+                    return false;
+                }
+                else
+                {
+                    return _child.Contains(s.Substring(1));
+                }
+            }
+        }
+        /// <summary>
+        /// Indicates whether the trie rooted at this node contains the empty string.
+        /// </summary>
+        private bool _hasEmptyString;
+
+        /// <summary>
+        /// Stores the only child
+        /// </summary>
+        private ITrie _child;
+
+        /// <summary>
+        /// stores the childs Label
+        /// </summary>
+        private char _childLabel;
+
+        /// <summary>
+        /// Constructs a trie containing the given string and having the given child at the given label.
+        /// If s contains any characters other than lower-case English letters,
+        /// throws an ArgumentException.
+        /// If childLabel is not a lower-case English letter, throws an ArgumentException.
+        /// </summary>
+        /// <param name="s">The string to include.</param>
+        /// <param name="hasEmpty">Indicates whether this trie should contain the empty string.</param>
+        /// <param name="childLabel">The label of the child.</param>
+        /// <param name="child">The child labeled childLabel.</param>
+        public TrieWithOneChild(string s, bool hasEmpty)
+        {
+            if (s== "" || s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            _hasEmptyString = hasEmpty;
+            _childLabel = s[0];
+            _child = new TrieWithNoChildren().Add(s.Substring(1));
+        }
+    }
+}
